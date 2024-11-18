6a1a4d98b3ddc3a650bcce4dc99ba6f5a5442aca
Ayrton Emanuel <ayrtonemanueldeveloper@gmail.com>
2024-11-18 09:22:06 -0300

    (feat): .NET Core arrange layers.

---
+++ Back/src/ProEventos.Persistence/Contratos/IPalestrantePersist.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Contratos
+{
+    public interface IPalestrantePersist
+    {
+        Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string tema, bool includeEventos);
+        Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos);
+        Task<Palestrante> GetPalestranteIdAsync(int palestranteId, bool includeEventos);
+    }
+}
